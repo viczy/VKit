b9a666c1c895b402ad63e3eb4b7214d3ac91409f
Zhouhaijian <viczy@ymail.com>
2014-11-14 17:49:25 +0800

    add the manager

--- VKit/VFileManager.swift
+++ VKit/VFileManager.swift
@@ -19,9 +19,9 @@ class VFileManager: NSObject {
 * @note
 */
 
-    class func fileExist(filepath:NSString) -> Bool{
+    class func fileExist(path:NSString) -> Bool{
         var fileManager = NSFileManager.defaultManager()
-        return fileManager.fileExistsAtPath(filepath)
+        return fileManager.fileExistsAtPath(path)
     }
 
 /**
@@ -33,5 +33,52 @@ class VFileManager: NSObject {
 * @return  是否删除成功
 * @note
 */
+    class func removeFile(path:NSString) -> Bool {
+        var error:NSError?
+        var fileManager = NSFileManager.defaultManager()
+        if (fileManager.fileExistsAtPath(path)) {
+            fileManager.removeItemAtPath(path, error:&error)
+        }
+
+        if let myError = error {
+            return false
+        }
+
+        return true
+    }
+
+/**
+* @brief 创建文件
+*
+* Detailed
+* @param[in]  文件路径
+* @param[out] N/A
+* @return  是否创建成功
+* @note
+*/
+
+    class func makeDir(path:NSString) -> Bool {
+        var error:NSError?
+        var fileManager = NSFileManager.defaultManager()
+        fileManager.createDirectoryAtPath(path, withIntermediateDirectories:true, attributes:nil, error:&error)
+        if let myError = error {
+            return false
+        }
+        return true
+    }
+
+/**
+* @brief 获取documents路径
+*
+* Detailed
+* @param[in]  N/A
+* @param[out] N/A
+* @return  documents路径
+* @note
+*/
+
+
+
+
 
 }
